5d28e0993867a285b99164d8ed7bc781c94f6c40
Micael Velez <mvrod2314@gmail.com>
2025-11-19 15:00:22 +0000

    mains

--- main.h
+++ main.h
@@ -1,5 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+#include <time.h>
+#include <float.h>
+#include <limits.h>
+#include <wctype.h>
 
 int _printf(const char *format, ...);
 
